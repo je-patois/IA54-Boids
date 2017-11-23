6f83190949ff9817f618c47656772af560178f2f
Mickael Constanzer <mickael.constanzer@laposte.net>
2017-11-23 18:19:11 +0100

    Création agent Boids

---
+++ core/src/main/generated-sources/sarl/fr/utbm/boids/core/IsStarted.java
@@ -0,0 +1,28 @@
+package fr.utbm.boids.core;
+
+import io.sarl.lang.annotation.SarlElementType;
+import io.sarl.lang.annotation.SarlSpecification;
+import io.sarl.lang.annotation.SyntheticMember;
+import io.sarl.lang.core.Address;
+import io.sarl.lang.core.Event;
+
+/**
+ * event pour prévenir l'environnement que le Boids est lancé
+ */
+@SarlSpecification("0.6")
+@SarlElementType(14)
+@SuppressWarnings("all")
+public class IsStarted extends Event {
+  @SyntheticMember
+  public IsStarted() {
+    super();
+  }
+  
+  @SyntheticMember
+  public IsStarted(final Address source) {
+    super(source);
+  }
+  
+  @SyntheticMember
+  private final static long serialVersionUID = 588368462L;
+}
